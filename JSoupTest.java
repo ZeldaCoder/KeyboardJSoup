dd53d39bd08647096b0b3f764acdd8c9445b4263
ZeldaCoder <jedediah.c.muthalaly@gmail.com>
2025-01-13 20:22:37 -0800

    adding as much as I can

--- JSoupTest.java
+++ JSoupTest.java
@@ -5,40 +5,50 @@ import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 import java.io.IOException;
+import java.util.ArrayList;
+
+import java.io.FileWriter;
 
 public class JSoupTest
 {
 
+    static ArrayList<ArrayList<String>> reviewData = new ArrayList<ArrayList<String>>();
+
+
+
  
      public static void main(String[] args) throws IOException {  
 
-      Document  doc = Jsoup.connect("https://amazon.com/")
-      .userAgent("Mozilla/5.0 (Windows; U; WindowsNT 5.1; en-US; rv1.8.1.6) Gecko/20070725 Firefox/2.0.0.6")
-      .referrer("http://www.google.com")
-      .get();
+        Document  doc = Jsoup.connect("https://www.bestbuy.com/site/reviews/corsair-k70-rgb-pro-full-size-wired-mechanical-cherry-mx-speed-linear-switch-gaming-keyboard-with-pbt-double-shot-keycaps-black/6491897?variant=A&skuId=6491897&rating=5%2C4%2C3%2C2%2C1")
+        .userAgent("Mozilla/5.0 (Windows; U; WindowsNT 5.1; en-US; rv1.8.1.6) Gecko/20070725 Firefox/2.0.0.6")
+        .referrer("http://www.google.com")
+        .timeout(1000)
+        .get();
+
         
-        Elements links = doc.select("a[href]");
-        Elements media = doc.select("[src]");
-        Elements imports = doc.select("link[href]");
+        
+        Elements reviewNames = doc.select("p");
+        Elements reviews = doc.select("[src]");
+        Elements dates = doc.select("link[href]");
 
-        print("\nMedia: (%d)", media.size());
-        for (Element src : media) {
-            if (src.nameIs("img"))
-                print(" * %s: <%s> %sx%s (%s)",
-                        src.tagName(), src.attr("abs:src"), src.attr("width"), src.attr("height"),
-                        trim(src.attr("alt"), 20));
-            else
-                print(" * %s: <%s>", src.tagName(), src.attr("abs:src"));
-        }
+        
 
-        print("\nImports: (%d)", imports.size());
-        for (Element link : imports) {
-            print(" * %s <%s> (%s)", link.tagName(),link.attr("abs:href"), link.attr("rel"));
-        }
+        try {
+            FileWriter writer = new FileWriter("socialMediaPosts.txt");
 
-        print("\nLinks: (%d)", links.size());
-        for (Element link : links) {
-            print(" * a: <%s>  (%s)", link.attr("abs:href"), trim(link.text(), 35));
+
+            for (int i = 0; i < reviewNames.size(); i++) {
+                ArrayList<String> data = new ArrayList<String>();
+                reviewData.add(data);
+                writer.write("#" + i + ": " + reviewData.get(i).get(0) + ", " + reviewData.get(i).get(1) + ", " + reviewData.get(i).get(2));
+            }
+            // Close the writer
+            writer.close();
+
+            System.out.println("Data written to file successfully.");
+        } catch (IOException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
         }
     }
 
